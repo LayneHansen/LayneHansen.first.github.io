258f9261ebeaee2b86d63bd217a98d8d6dc6615a
LayneHansen <layne.d.hansen@gmail.com>
2021-02-14 14:19:14 -0700

    updated contact page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,11 +6,12 @@
 
 // Text field 
 
-// $("#contact-submit").on(click, function () {
-//     event.preventDefault();
+$("#contact-submit").on(click, function () {
+    event.preventDefault();
 
-//     firstName = $("#first-name").val().trim();
-//     lastName = $("#last-name").val().trim();
-//     email = $("#email").val().trim();
-//     textArea = $("#text-area").val().trim();
-// })
+    firstName = $("#first-name").val().trim();
+    console.log(firstName)
+    lastName = $("#last-name").val().trim();
+    email = $("#email").val().trim();
+    textArea = $("#text-area").val().trim();
+})
